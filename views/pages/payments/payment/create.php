558dc46cd3d75315f2d3cbfa929c10fab6fd567d
[devmizan] <[hellomizanur247@gmail.com]>
2024-12-15 13:43:48 +0600

    remove comment in payment create page

--- views/pages/payments/payment/create.php
+++ views/pages/payments/payment/create.php
@@ -1,18 +1,18 @@
 <?php
-// echo Page::title(["title"=>"Create Payment"]);
-// echo Page::body_open();
-// echo Html::link(["class"=>"btn btn-success", "route"=>"payment", "text"=>"Manage Payment"]);
-// echo Page::context_open();
-// echo Form::open(["route"=>"payment/save"]);
-// 	echo Form::input(["label"=>"Booking","name"=>"booking_id","table"=>"bookings"]);
-// 	echo Form::input(["label"=>"Amount","type"=>"text","name"=>"amount"]);
-// 	echo Form::input(["label"=>"Payment Method","type"=>"text","name"=>"payment_method"]);
-// 	echo Form::input(["label"=>"Payment Status","type"=>"text","name"=>"payment_status"]);
+echo Page::title(["title"=>"Create Payment"]);
+echo Page::body_open();
+echo Html::link(["class"=>"btn btn-success", "route"=>"payment", "text"=>"Manage Payment"]);
+echo Page::context_open();
+echo Form::open(["route"=>"payment/save"]);
+	echo Form::input(["label"=>"Booking","name"=>"booking_id","table"=>"bookings"]);
+	echo Form::input(["label"=>"Amount","type"=>"text","name"=>"amount"]);
+	echo Form::input(["label"=>"Payment Method","type"=>"text","name"=>"payment_method"]);
+	echo Form::input(["label"=>"Payment Status","type"=>"text","name"=>"payment_status"]);
 
-// echo Form::input(["name"=>"create","class"=>"btn btn-primary offset-2", "value"=>"Save", "type"=>"submit"]);
-// echo Form::close();
-// echo Page::context_close();
-// echo Page::body_close();
+echo Form::input(["name"=>"create","class"=>"btn btn-primary offset-2", "value"=>"Save", "type"=>"submit"]);
+echo Form::close();
+echo Page::context_close();
+echo Page::body_close();
 
 
 ?>
